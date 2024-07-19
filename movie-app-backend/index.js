ed272806c6e9ba48fbb21b49cdf0484ffecaa378
alain descartes <ualaindescartes@gmail.com>
2024-07-18 19:50:27 -0600

    pushed frontend

--- movie-app-backend/index.js
+++ movie-app-backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const connectDB = require("./src/config/connectDb");
 const cors = require("cors");
 const authRouter = require("./src/routes/authRoute");
@@ -10,7 +11,7 @@ const allowedOrigins = ["http://localhost:3000", "http://localhost:8000"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true); // Allow the origin in the response if it's in the allowed list
+      callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
@@ -22,33 +23,36 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve Static Files from React App
+app.use(express.static(path.join(__dirname, '..', 'movie-app', 'build')));
+
 //ROUTES
 app.use("/api/auth", authRouter);
 
 // Connect to the database
 connectDB()
-  .then(() => {
-    console.log("Ready to handle requests!");
+    .then(() => {
+      console.log("Ready to handle requests!");
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+      // All remaining requests return the React app, so it can handle routing.
+      app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'movie-app', 'build', 'index.html'));
+      });
+
+      app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+      });
+    })
+    .catch((err) => {
+      console.error("Failed to connect to the database:", err);
     });
-  })
-  .catch((err) => {
-    console.error("Failed to connect to the database:", err);
-  });
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
-  // Log the error internally
   console.error(err);
-
-  // Use the statusCode from the error if it exists, otherwise default to 500
   const statusCode = err.statusCode || 500;
-
-  // Send the error message back to the client
   res.status(statusCode).json({
-    status: statusCode >= 500 ? "error" : "fail", // 'error' for server errors, 'fail' for client errors
+    status: statusCode >= 500 ? "error" : "fail",
     message: err.message,
   });
 });
